7c389c51d5bc6bae5502d32c4ea80e25e8f3dc98
Aaron Merriam <amerriam@fusionbox.com>
2012-09-12 13:56:34 -0600

    Updated admin class to dynamically generate readonly fields

--- fusionbox/contact/admin.py
+++ fusionbox/contact/admin.py
@@ -6,26 +6,52 @@ from fusionbox.admin import CsvAdmin
 
 
 class SubmissionAdmin(admin.ModelAdmin, CsvAdmin):
+    """
+    Default admin class for ``fusionbox.contact.models.Submission``.  Allows
+    for bulk csv export of ``Submission`` entries, and restricts readonly
+    access.
+    """
     list_display = ('name', 'email', 'list_comment', 'created_at')
-    readonly_fields = ('created_at', 'name', 'email', 'comment')
-
-    class Meta:
-        model = Submission
+    date_hierarchy = 'created_at'
 
     class Media:
+        """
+        Ensures that whitespace linebreaks are respected when displaying the
+        ``Submission.comment`` field.
+        """
         css = {
                 'all': ('contact.css',)
                 }
 
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Dynamically build a list of field names for the model.  This allows for
+        monkeypatches to the ``Submission`` model to not require a new admin
+        class.
+        """
+        return [field.attname for field in self.model._meta.fields if not field.primary_key]
+
     def list_comment(self, submission):
+        """
+        Allows for display of a truncated version of ``Submission.comment`` in
+        the list_display.
+        """
         return submission.comment[:200]
     list_comment.short_description = 'Comment'
 
     def has_add_permission(self, *args, **kwargs):
+        """
+        Submissions should not be creatable from the admin center.
+        """
         return False
 
 
 class RecipientAdmin(admin.ModelAdmin):
+    """
+    Defauld admin class for ``fusionbox.contact.models.Recipient``.  Only
+    registered if no hard-coded list of recipients is found in
+    ``settings.CONTACT_FORM_RECIPIENTS``.
+    """
     list_display = ('name', 'email', 'is_active')
     fieldsets = (
                 (None, {
@@ -34,11 +60,9 @@ class RecipientAdmin(admin.ModelAdmin):
                     }),
             )
 
-    class Meta:
-        model = Recipient
-
 admin.site.register(Submission, SubmissionAdmin)
 
+# Only register the Recipient model if no hard-coded list of recipients was found
 if hasattr(settings, 'CONTACT_FORM_RECIPIENTS'):
     pass
 else:

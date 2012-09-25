0b8843edfb6844de1d50611195fd99fabcf8f072
Aaron Merriam <amerriam@fusionbox.com>
2012-09-25 14:32:35 -0600

    added error and required css classes

--- fusionbox/contact/forms.py
+++ fusionbox/contact/forms.py
@@ -4,6 +4,9 @@ from fusionbox.contact.models import Submission
 
 
 class ContactForm(UncaptchaModelForm):
+    error_css_class = 'error'
+    required_css_class = 'required'
+
     class Meta:
         model = Submission
         exclude = ('created_at',)
